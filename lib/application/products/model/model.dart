65fd5a8ee63df934a206f0f9c3a7244504fcb9a4
devsr143 <devachandsr.143@gmail.com>
2026-06-23 14:08:09 +0530

    model added

---
+++ lib/application/products/model/model.dart
@@ -0,0 +1,102 @@
+class ProductModel {
+  int? id;
+  String? title;
+  String? slug;
+  String? description;
+  Category? category;
+  List<String>? images;
+  String? creationAt;
+  String? updatedAt;
+
+  ProductModel({
+    required this.id,
+    required this.title,
+    required this.slug,
+    required this.description,
+    required this.category,
+    required this.images,
+    required this.creationAt,
+    required this.updatedAt,
+  });
+  factory ProductModel.fromJson(Map<String, dynamic> Json) {
+    return ProductModel(
+      id: Json['id'],
+      title: Json['title'],
+      slug: Json['slug'],
+      description: Json['description'],
+      category: Category.fromJson(Json['category']),
+      images: List<String>.from(Json['images']),
+      creationAt: Json['creationAt'],
+      updatedAt: Json['updatedAt'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'slug': slug,
+      'description': description,
+      'category': category?.toJson(),
+      'images': images,
+      'creationAt': creationAt,
+      'updatedAt': updatedAt,
+    };
+  }
+}
+
+class Category {
+  int? id;
+  String? name;
+  String? image;
+
+  Category({required this.id, required this.name, required this.image});
+
+  factory Category.fromJson(Map<String, dynamic> Json) {
+    return Category(id: Json['id'], name: Json['name'], image: Json['image']);
+  }
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'name': name, 'image': image};
+  }
+}
+
+
+// class Category {
+//   int? id;
+//   String? name;
+//   String? slug;
+//   String? image;
+//   String? creationAt;
+//   String? updatedAt;
+
+//   Category({
+//     required this.id,
+//     required this.name,
+//     required this.slug,
+//     required this.image,
+//     required this.creationAt,
+//     required this.updatedAt,
+//   });
+//   factory Category.fromJson(Map<String, dynamic> Json) {
+//     return Category(
+//       id: Json['id'],
+//       name: Json['name'],
+//       slug: Json['slug'],
+//       image: Json['image'],
+//       creationAt: Json['creationAt'],
+//       updatedAt: Json['updatedAt'],
+//     );
+//   }
+//   Map<String, dynamic> toJson() {
+//     return {
+//       'id': id,
+//       'name': name,
+//       'slug': slug,
+//       'image': image,
+//       'creationAt': creationAt,
+//       'updatedAt': updatedAt,
+//     };
+//   }
+// }
+
+
